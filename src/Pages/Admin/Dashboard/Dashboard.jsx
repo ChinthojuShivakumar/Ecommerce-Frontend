2f53c327cb0a8c8f58bd13504b1cc858a611cb09
CHINTHOJU SHIVAKUMAR <shivakumarchinthoju@gmail.com>
2025-06-06 15:57:31 +0530

    dashboard admin

--- src/Pages/Admin/Dashboard/Dashboard.jsx
+++ src/Pages/Admin/Dashboard/Dashboard.jsx
@@ -1,27 +1,58 @@
-import React from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import style from "./dashboard.module.css";
 import SideMenu from "../../../Components/Admin/Sidemenu/Sidemenu";
 import Header from "../../../Components/Layout/Header";
+import { axiosInstanceV1 } from "../../../Utils/ApiServices";
 
 const Dashboard = () => {
+  const [products, setProducts] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [bookings, setBookings] = useState([]);
+  const [category, setCategory] = useState([]);
+
+  const fetchDashboard = async () => {
+    try {
+      const response = await Promise.all([
+        axiosInstanceV1.get("/product"),
+        axiosInstanceV1.get("/user"),
+        axiosInstanceV1.get("/booking"),
+        axiosInstanceV1.get("/category"),
+      ]);
+      const [products, users, bookings, category] = response.map(
+        (res) => res.data
+      );
+
+      setProducts(products.productList);
+      setBookings(bookings.bookingList);
+      setCategory(category.categoryList);
+      setUsers(users.userList);
+    } catch (error) {
+      return error;
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboard();
+  }, []);
   const COUNT_LIST = [
     {
       name: "Total Categories",
-      count: 10,
+      count: category.length,
     },
     {
       name: "Total Users",
-      count: 10,
+      count: users.length,
     },
     {
       name: "Total Products",
-      count: 10,
+      count: products.length,
     },
     {
       name: "Total Bookings",
-      count: 10,
+      count: bookings.length,
     },
   ];
+
   return (
     <div>
       <Header />
@@ -32,7 +63,7 @@ const Dashboard = () => {
         <div className={style.body}>
           <h1>Dash board home</h1>
           <div className={style.card}>
-            {COUNT_LIST.map((field,i) => {
+            {COUNT_LIST.map((field, i) => {
               return (
                 <ul key={i}>
                   <div className={style.cardlist}>
