47044820600ae270be0785bfeaf469ccb9350cf6
CHINTHOJU SHIVAKUMAR <shivakumarchinthoju@gmail.com>
2025-08-01 10:49:47 +0530

    product display

--- src/Utils/ApiServices.jsx
+++ src/Utils/ApiServices.jsx
@@ -1,12 +1,10 @@
 import axios from "axios";
 import { errorMessage, successMessage } from "./Alert";
 
-
 // export const TEST_BASE_URL = "http://localhost:8082/v1";
 // export const TEST_HOST_URL = "http://localhost:5173";
 
-// export const PRODUCTION_BASE_URL =
-//   "https://ecommerce-backend-tng6.onrender.com/v1";
+// const BASE_URL = "https://ecommerce-backend-tng6.onrender.com/v1";
 // export const PRODUCTION_HOST_URL =
 //   "https://instantdeliveryservices.vercel.app/";
 
@@ -98,7 +96,6 @@ const userRole = localStorage.getItem("role");
 console.log("AXIOS TOKEN", userToken);
 console.log("ROLE", userRole);
 console.log(BASE_URL, "BASE_URL");
-
 
 export const axiosInstanceV1 = axios.create({
   baseURL: BASE_URL,
